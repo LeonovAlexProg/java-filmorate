06681f34c9aad5c12e014d8fe32d7b297ed919cf
Alex <leonovalexprog@gmail.com>
2023-04-27 22:36:07 +0300

    upd: test commit

--- src/main/java/ru/yandex/practicum/filmorate/FilmorateApplication.java
+++ src/main/java/ru/yandex/practicum/filmorate/FilmorateApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FilmorateApplication {
 
+//конфлит для меррджа
 	public static void main(String[] args) {
 		SpringApplication.run(FilmorateApplication.class, args);
 	}
